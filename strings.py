31ab3c51c5bf21ec3eb80052305b2c523f638b8e
Aman Sharma <aman.11300733@lpu.in>
2017-09-05 12:56:44 +0530

    added images and neccessary files

---
+++ strings.py
@@ -0,0 +1,44 @@
+# print(10 ** 3)
+# firstname = 'Aman '
+# secondname = 'Sharma'
+# print(firstname + secondname)
+
+# exmapleVar = print(3+4)
+
+# x,y=(3,7,8)
+# print(x)
+# print(y)
+
+# Loops
+# i=1;
+# while(i<=10):
+# 	print(i)
+# 	i+=1
+
+# while True:
+# 	print('Hello')
+
+# for(i=1;i<10;i++)
+# 	print('great')
+
+# Numbers = [1,2,3,4,5,6,7,8,9,10,11,'Aman']
+
+# for x in Numbers:
+# 	print(x)
+# 	print('Hello')
+
+# for x in range(1,11):
+# 	print(x)
+
+x=11
+y=21
+z=20
+if x>y:
+	print(x)
+else:
+	print(y)
+
+# i=1
+# while i<10:
+# 	print(i)
+# 	i+=1
